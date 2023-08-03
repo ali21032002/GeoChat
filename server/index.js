d870d19262dd7039dc024b9f6d222a4de2ab6a74
ali21032002 <asalimi@gmail.com>
2023-08-03 23:22:07 +0330

    add socket io handler

--- server/index.js
+++ server/index.js
@@ -2,21 +2,33 @@ const PORT = process.env.PORT || 3003;
 
 const express = require('express');
 const http = require('http');
+const { Server } = require('socket.io');
 const cors = require('cors');
-const socket = require('socket.io');
 
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
 
-app.use(cors());
-app.get('/', (req, res) => {
-    res.send('Hello world ');
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(`new user is connected with id ${socket.id}`);
+
+    socket.on('disconnect', () => {
+        socketDisconnectHandler(socket.id);
+    });
 });
 
 server.listen(PORT, () => {
     console.log(`server is up on port ${PORT}`);
 });
 
-
-
-
+const socketDisconnectHandler = (socketId) => {
+    console.log(`user by disconnected by id :  ${socketId}`)
+}
