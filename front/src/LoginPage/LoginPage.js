e2677a44a5ee586454b52e29b3b4d0b14a300404
ali21032002 <asalimi@gmail.com>
2023-08-04 20:05:09 +0330

    navigations and location

--- front/src/LoginPage/LoginPage.js
+++ front/src/LoginPage/LoginPage.js
@@ -1,22 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LoginButton from './loginComponents/loginButton';
 import LoginInput from './loginComponents/loginInput';
 import Logo from './loginComponents/logo';
-
 import './LoginPage.css';
 
 const isUsernameValid = (username) => {
     return username.length > 0 && username.length < 10 && !username.includes(' ');
 }
 
-export default function LoginPage() {
+
+const locationOptions = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+}
+
+const LoginPage = () => {
     const [userName, setUserName] = useState('');
+
+    const [locationErrorOccurred, setLocationErrorOccurred] = useState(false);
+
     const navigate = useNavigate();
 
     const handleLogin = () => {
         navigate('/map');
     }
+
+    const onSuccess = (position) => {
+        console.log(position)
+    };
+
+    const onError = (error) => {
+        console.log('Error oraced on get Location : ');
+        console.log(error);
+
+        setLocationErrorOccurred(true);
+
+    };
+
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            onSuccess,
+            onError,
+            locationOptions
+        )
+    }, []);
 
     return (
         <div className='l_page_main_container'>
@@ -25,8 +55,10 @@ export default function LoginPage() {
                 <LoginInput userName={userName} setUserName={setUserName} />
                 <LoginButton
                     onClickHandler={handleLogin}
-                    disabled={!isUsernameValid(userName)} />
+                    disabled={!isUsernameValid(userName) || locationErrorOccurred} />
             </div>
         </div>
     )
 }
+export default LoginPage;
+
